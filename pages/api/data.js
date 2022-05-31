a201ef1c4df2dc502e97ae0c17ec9af337ec183a
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-05-31 13:53:22 +0100

    Added api to index.js

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,32 +1,39 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
 
 var mysql      = require('mysql');
-import { host, database, user, password } from '../../lib/constant';
 
-var connection = mysql.createConnection({
-  host     : host,
-  user     : user,
-  password : password,
-  database : database
-});
-
-connection.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
-    }
-    // console.log('MySQL Connection Successful.');
-  });
 
 // http://localhost:3000/api/data?table=planes
 export default function handler(req, res) {
-  const table_name =req.query.table
+    // var
+    const table_name =req.query.table
+    const db = req.query.db
+    const host = req.query.host
+    const user = req.query.user
+    const pwd = req.query.pwd
+    
+    // connection
+    var connection = mysql.createConnection({
+      host     : host,
+      user     : user,
+      password : pwd,
+      database : db
+    });
+  
+    // then, connect()
+    connection.connect(function(err) {
+      if (err) {
+        return console.error('error: ' + err.message);
+      }
+      // console.log('MySQL Connection Successful.');
+    });
+  
 
   const user_query = 'SELECT * FROM '+table_name;
   
   connection.query(user_query, function (error, result, fields){
       if (error) throw error;
+
       res.status(200).json({ data: result})
   })
-  // connection.end()
+  connection.end()
 }
